513e4cf2831b41cdcc8ad03816af0d2c61280955
sowinskipatryk <sowinski.patryk@wp.pl>
2024-12-22 23:45:56 +0100

    added custom formatter to align logs on different levels

--- log/config.py
+++ log/config.py
@@ -2,20 +2,29 @@ import logging
 
 # Create a logger object
 logger = logging.getLogger('logger')
+log_level = logging.DEBUG
 
 # Set the logging level (DEBUG, INFO, WARNING, ERROR, CRITICAL)
-logger.setLevel(logging.INFO)
+logger.setLevel(log_level)
 
 # Create a file handler and set its level
 file_handler = logging.FileHandler('exploding_kittens.log')
-file_handler.setLevel(logging.INFO)
+file_handler.setLevel(log_level)
 
 # Create a console handler and set its level
 console_handler = logging.StreamHandler()
-console_handler.setLevel(logging.INFO)
+console_handler.setLevel(log_level)
+
+
+# Create a custom formatter class to align messages on different log levels
+class FixedWidthFormatter(logging.Formatter):
+    def format(self, record):
+        record.levelname = f"{record.levelname:<5}"  # 5 is enough as I don't use WARNING or CRITICAL in this project
+        return super().format(record)
+
 
 # Create a formatter and set its format
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+formatter = FixedWidthFormatter('%(asctime)s [%(levelname)s] %(message)s')
 file_handler.setFormatter(formatter)
 console_handler.setFormatter(formatter)
 
